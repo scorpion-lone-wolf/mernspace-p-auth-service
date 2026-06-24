0260550e2f7c5d94c76a714288f9f738136cde5a
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-06-24 12:25:41 +0530

    add test setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import expres, { NextFunction, Request, Response } from "express";
-import createHttpError, { HttpError } from "http-errors";
+import { HttpError } from "http-errors";
 import logger from "./config/logger";
 
 const app = expres();
 
 app.get("/", async (req, res, next) => {
-  const err = createHttpError(404, "Not Found");
-  throw err;
-  // res.send("Welcome to Auth Service");
+  res.send("Welcome to Auth Service");
 });
 
 // global error handler (it has 4 param which help express distinguish between normal middleware and global error handler)
