ed6cc1885fd02d473cdda8c82dd7b0a5d0f85fff
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-06-24 11:27:34 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import expres from "express";
+import expres, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app = expres();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Auth Service");
+app.get("/", async (req, res, next) => {
+  const err = createHttpError(404, "Not Found");
+  throw err;
+  // res.send("Welcome to Auth Service");
+});
+
+// global error handler (it has 4 param which help express distinguish between normal middleware and global error handler)
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        message: err.message,
+        path: "", // url path where error has occur
+        location: "" // location like line number where error has occur
+      }
+    ]
+  });
 });
 
 export default app;
