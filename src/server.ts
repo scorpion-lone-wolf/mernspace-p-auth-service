c3a3d910b3da54284823e74b30a5771c7d10f7ed
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-06-29 09:59:42 +0530

    add db connection to server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/dataSource";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
   const PORT = Config.PORT;
   try {
+    await AppDataSource.initialize();
+    logger.info("Database connected");
     app.listen(PORT, () => {
       logger.info(`Auth Service Started on PORT:${PORT}`);
     });
   } catch (error) {
+    logger.error(error);
     console.error(error);
     process.exit(1);
   }
